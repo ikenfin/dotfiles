94eadad6d4f46c416dc0c75291a2d7e27c86f868
ikenfin <ikenfin@gmail.com>
2022-12-27 16:31:33 +0400

    add zsh autosuggestions

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -9,3 +9,5 @@ sh ~/.tmux/plugins/tpm/scripts/install_plugins.sh
 # Install starship
 curl -sS https://starship.rs/install.sh | sh
 
+# Install zsh autosuggestion
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
